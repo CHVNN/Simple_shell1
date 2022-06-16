5628c4500cf9c47c5f6c7ca40cca851d6e8ffac8
CHAN <93952943+CHVNN@users.noreply.github.com>
2022-06-16 23:19:06 +0100

    Update exit.c

--- exit.c
+++ exit.c
@@ -1,16 +1,34 @@
+
 #include "shell.h"
 
 /**
- *exit_shell - Exits the shell with exit status specified
+ * exit_shell - This code is used to
+ * exit the shell
  *
- *@datash: SHell data needed to exit
- *Return: An integer value to specify success
+ * @datash: data relevant
+ * (status and args)
+ * Return: 0 on success.
  */
 int exit_shell(data_shell *datash)
 {
-	char *str = "Work still in progress";
+	unsigned int ustatus;
+	int is_digit;
+	int str_len;
+	int big_number;
 
-	(void)datash;
-	printf("%s", str);
+	if (datash->args[1] != NULL)
+	{
+		ustatus = _atoi(datash->args[1]);
+		is_digit = _isdigit(datash->args[1]);
+		str_len = _strlen(datash->args[1]);
+		big_number = ustatus > (unsigned int)INT_MAX;
+		if (!is_digit || str_len > 10 || big_number)
+		{
+			get_error(datash, 2);
+			datash->status = 2;
+			return (1);
+		}
+		datash->status = (ustatus % 256);
+	}
 	return (0);
 }
