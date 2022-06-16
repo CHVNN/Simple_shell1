c338b25298e34ecf93f1ff47d3f6f6750b08d613
CHAN <93952943+CHVNN@users.noreply.github.com>
2022-06-16 23:26:55 +0100

    Update get_line.c

--- get_line.c
+++ get_line.c
@@ -1,29 +1,88 @@
 #include "shell.h"
 
 /**
- *get_line - Gets the command line entered by the user
- *@datash: Struct containing shell information
- *Return: thenumber of bytes read is returned
+ * bring_line - This is used in
+ * assigning the line var
+ * for the get_line
+ * @lineptr: Buffer that store
+ * the input str
+ * @buffer: str that is been called to line
+ * @n: size of line
+ * @j: size of buffer
  */
-ssize_t get_line(data_shell *datash)
+void bring_line(char **lineptr, size_t *n, char *buffer, size_t j)
 {
-	ssize_t len;
-	char *ptr_n, *ptr_t;
-	size_t bufferSize = 0;
 
-	datash->args = NULL;
-	datash->counter++;
-	if (isatty(STDIN_FILENO))
-		type_prompt();
-	len = getline(&datash->input, &bufferSize, stdin);
-	if (len == EOF)
+	if (*lineptr == NULL)
 	{
-		free_mem(datash);
-		exit(0);
+		if  (j > BUFSIZE)
+			*n = j;
+
+		else
+			*n = BUFSIZE;
+		*lineptr = buffer;
 	}
-	ptr_n = _strchr(datash->input, '\n');
-	ptr_t = _strchr(datash->input, '\t');
-	if (ptr_n || ptr_t)
-		insertNullByte(datash->input, len - 1);
-	return (len);
+	else if (*n < j)
+	{
+		if (j > BUFSIZE)
+			*n = j;
+		else
+			*n = BUFSIZE;
+		*lineptr = buffer;
+	}
+	else
+	{
+		_strcpy(*lineptr, buffer);
+		free(buffer);
+	}
+}
+/**
+ * get_line - This code is used to
+ * Read inpt from stream
+ * @lineptr: buffer that stores the input
+ * @n: size of lineptr
+ * @stream: stream to read from
+ * Return: The number of bytes
+ */
+ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
+{
+	int i;
+	static ssize_t input;
+	ssize_t retval;
+	char *buffer;
+	char t = 'z';
+
+	if (input == 0)
+		fflush(stream);
+	else
+		return (-1);
+	input = 0;
+
+	buffer = malloc(sizeof(char) * BUFSIZE);
+	if (buffer == 0)
+		return (-1);
+	while (t != '\n')
+	{
+		i = read(STDIN_FILENO, &t, 1);
+		if (i == -1 || (i == 0 && input == 0))
+		{
+			free(buffer);
+			return (-1);
+		}
+		if (i == 0 && input != 0)
+		{
+			input++;
+			break;
+		}
+		if (input >= BUFSIZE)
+			buffer = _realloc(buffer, input, input + 1);
+		buffer[input] = t;
+		input++;
+	}
+	buffer[input] = '\0';
+	bring_line(lineptr, n, buffer, input);
+	retval = input;
+	if (i != 0)
+		input = 0;
+	return (retval);
 }
